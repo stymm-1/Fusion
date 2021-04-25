7b07ab09d8912f09e036e2f568ba5fff36071680
Satyam Gupta <64557006+stymm-1@users.noreply.github.com>
2021-04-25 13:35:06 +0530

    Update leaders.js

--- src/data/leaders.js
+++ src/data/leaders.js
@@ -2,7 +2,7 @@ export const LEADERS = [
     {
       id: 0,
       name: 'Professor',
-      image: 'Fusion/assets/images/alberto.png',
+      image: '/assets/images/alberto.png',
       designation: 'Chief Epicurious Officer',
       abbr: 'CEO',
       featured: false,
@@ -11,7 +11,7 @@ export const LEADERS = [
     {
       id: 1,
       name: 'Berlin',
-      image: 'Fusion/assets/images/alberto.png',
+      image: '/assets/images/alberto.png',
       designation: 'Chief Food Officer',
       abbr: 'CFO',
         featured: false,
@@ -20,7 +20,7 @@ export const LEADERS = [
     {
       id: 2,
       name: 'Tokyo',
-      image: 'Fusion/assets/images/alberto.png',
+      image: '/assets/images/alberto.png',
       designation: 'Chief Taste Officer',
       abbr: 'CTO',
         featured: false,
@@ -29,7 +29,7 @@ export const LEADERS = [
     {
       id: 3,
       name: 'Raquel Murillo',
-      image: 'Fusion/assets/images/alberto.png',
+      image: '/assets/images/alberto.png',
       designation: 'Executive Chef',
       abbr: 'EC',
       featured: true,
